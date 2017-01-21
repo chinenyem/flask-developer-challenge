bd96883db75581593c0ab93843510b490ab86864
Chinenyem Nwadiugwu <chnw1947@gmail.com>
2017-01-21 16:04:14 -0500

    completed coding challenge

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -8,7 +8,7 @@ module implements a Flask server exposing two endpoints: a simple ping
 endpoint to verify the server is up and responding and a search endpoint
 providing a search across all public Gists for a given Github account.
 """
-
+import urllib3
 import requests
 from flask import Flask, jsonify, request
 
@@ -41,9 +41,18 @@ def gists_for_user(username):
             username=username)
     response = requests.get(gists_url)
     # BONUS: What failures could happen?
+    # using the try and expect statement check if if a bad request is made
+    try:
+        response.raise_for_status()
+    # catch any HTTP errors, failures
+    except requests.excpetions.HTTPError as e:
+        print "Error: " + str(e)
     # BONUS: Paging? How does this work for users with tons of gists?
-
-    return response.json()
+    try:
+        response.json()
+        return response.json()
+    except Exception as e:
+        print "Error: " + str(e)
 
 
 @app.route("/api/v1/search", methods=['POST'])
@@ -60,26 +69,59 @@ def search():
     """
     post_data = request.get_json()
     # BONUS: Validate the arguments?
+    # Loop through dictionary and check if key valuse username 
+    # and pattern exists. If not return string "invalid arguments"
+    if all (a in post_data for a in ("username","pattern")):
+        username = post_data['username']
+        pattern = post_data['pattern']
 
-    username = post_data['username']
-    pattern = post_data['pattern']
+        result = {}
+        successful_matches_list = []
+        gists = gists_for_user(username)
+        # BONUS: Handle invalid users?
+        # check if there are gists else print "invalid users"
+        if gists:
+            for gist in gists:
+                # REQUIRED: Fetch each gist and check for the pattern
+                # on each loop the following happens:
+                # search for specific gist by id with the requested pattern parameter
+                # create url string, with the correct gist['id'] and pattern, using the positional formatting with the .format method
+                # using the request module, make a get request from gistapi to github to recieve a response object
+                # using the builtin JSON decoder from the response module to get the json dictionary 
+                # if there is successful request producing an response then append that reponse dictionary
+                # to the successful_matches_list type list. 
+                gist_id = gist['id']
+                r = 'https://api.github.com/gists/{gist_id}?q={pattern}'.format(gist_id=gist_id, pattern=pattern)
+                # check for any issues in a try and except
+                # checking for timeouts, many redirects, plain ole failures
+                try:
+                    response = requests.get(r)
+                except requests.exceptions.TooManyRedirects:
+                    # Tell the user their URL was bad and try a different one
+                    print "There are too many redirects. You have used a bad URL, please try a different one."
+                except requests.exceptions.RequestException as e:
+                    # catastrophic error. bail.
+                    print e
+                if response.json():
+                    response_json = response.json()
+                    successful_matches_list.append(response_json)
+                else:
+                    print "No matches"
+                # BONUS: What about huge gists?
+                # BONUS: Can we cache results in a datastore/db?
+                pass
 
-    result = {}
-    gists = gists_for_user(username)
-    # BONUS: Handle invalid users?
+            result['status'] = 'success'
+            result['username'] = username
+            result['pattern'] = pattern
+            result['matches'] = successful_matches_list
 
-    for gist in gists:
-        # REQUIRED: Fetch each gist and check for the pattern
-        # BONUS: What about huge gists?
-        # BONUS: Can we cache results in a datastore/db?
-        pass
-
-    result['status'] = 'success'
-    result['username'] = username
-    result['pattern'] = pattern
-    result['matches'] = []
-
-    return jsonify(result)
+            #print jsonify(result)
+            return jsonify(result)
+        else:
+            return "invalid users from Github"
+    else:
+        return "invalid arguments"
 
 
 if __name__ == '__main__':
